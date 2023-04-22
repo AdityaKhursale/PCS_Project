7a3d42e6761bd84f96b21dba1c0492113bd8c5b7
gauravdhus <gauravdhus@gmail.com>
2023-04-22 16:56:19 -0400

    Add DB connection support.

---
+++ src/sql/db.py
@@ -0,0 +1,52 @@
+import mysql.connector
+from mysql.connector import errorcode
+from table_schemas import TABLES
+
+
+def db_connection(db_user="pcs", db_pswd=""):
+    conn = mysql.connector.connect(user=db_user, password = db_pswd, host = "localhost")
+    return conn, conn.cursor()
+
+
+def create_db(cursor, db_name):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(db_name))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+
+def db_conn(db_name):
+    conn, cursor = db_connection()
+    try:
+        cursor.execute("USE {}".format(db_name))
+    except mysql.connector.Error as err:
+        print("Database {} does not exists.".format(db_name))
+        if err.errno == errorcode.ER_BAD_DB_ERROR:
+            create_db(cursor, db_name)
+            print("Database {} created successfully.".format(db_name))
+            conn.database = db_name
+        else:
+            print(err)
+            exit(1)
+
+    return conn, cursor
+
+
+def create_dfs_tables(cursor):
+    for table_name in TABLES:
+        table_description = TABLES[table_name]
+        try:
+            print("Creating table {}: ".format(table_name), end='')
+            cursor.execute(table_description)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print(f"\n\t -> already exists.")
+            else:
+                print(err.msg)
+        else:
+            print("OK")
+
+conn, cursor = db_conn("test")
+create_dfs_tables(cursor)
---
+++ src/sql/table_schemas.py
@@ -0,0 +1,28 @@
+TABLES={}
+
+TABLES['owned_filed_details'] = (
+    "CREATE TABLE `file_details` ("
+    " `file_id` varchar(255) NOT NULL,"
+    " `path` varchar(255) NOT NULL,"
+    " `public_key` varchar(2048) NOT NULL,"
+    " `private_key` varchar(2048) NOT NULL,"
+    "  PRIMARY KEY (`file_id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['replicated_file_details'] = (
+    "CREATE TABLE `file_details` ("
+    " `file_id` varchar(256) NOT NULL,"
+    " `owner` varchar(256) NOT NULL,"
+    " `path` varchar(256) NOT NULL,"
+    "  PRIMARY KEY (`file_id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['file_permissions'] = (
+    "CREATE TABLE `file_details` ("
+    " `file_id` varchar(256) NOT NULL,"
+    " `owner` varchar(256) NOT NULL,"
+    " `permission_write` boolean NOT NULL DEFAULT 0,"
+    " `public_key` varchar(2048) NOT NULL,"
+    " `private_key` varchar(2048) NOT NULL,"
+    "  PRIMARY KEY (`file_id`)"
+    ") ENGINE=InnoDB")
